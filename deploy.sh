fbce5d2b08f4ced03e3fcb24e97330f014d484c2
Gabriel Lacroix <lacroixgabriel@gmail.com>
2021-01-07 12:07:39 -0500

    Move deploy.sh to a git pre-push hook

--- deploy.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-bundle exec jekyll build &&
-cd _site/ &&
-git add . &&
-timestamp=$(date +"%Y-%m-%d") &&
-git commit -m "automated deployment $timestamp" &&
-git push origin gh-pages &&
-cd ..
