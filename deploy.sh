bd3da536d7142320cdea2a63c49e08ee4d4b36ae
Gabriel Lacroix <lacroixgabriel@gmail.com>
2021-01-08 10:29:04 -0500

    Update deploy script

--- deploy.sh
+++ deploy.sh
@@ -2,7 +2,7 @@
 bundle exec jekyll build &&
 cd _site/ &&
 git add . &&
-timestamp=$(date +"%Y-%m-%d") &&
+timestamp=$(date +"%F (%T)") &&
 git commit -m "automated deployment $timestamp" &&
 git push origin gh-pages &&
 cd ..
